562aa47367aced8044a3fd0ba1fd27a7eff9f39f
SimbaWANGU <simbawangulu@gmail.com>
2022-05-06 12:46:10 +0300

    Test

--- public/about.js
+++ public/about.js
@@ -4,12 +4,14 @@ document.querySelector('.hamburger_icon').addEventListener('click', () => {
     section.classList.toggle('blur');
   });
 });
+
 document.querySelector('.close_icon').addEventListener('click', () => {
   document.querySelector('.menu_item_container').classList.toggle('show');
   document.querySelectorAll('.mobile_background').forEach((section) => {
     section.classList.toggle('blur');
   });
 });
+
 document.querySelectorAll('.navlist-item-mobile').forEach((link) => {
   link.addEventListener('click', () => {
     document.querySelector('.menu_item_container').classList.toggle('show');
